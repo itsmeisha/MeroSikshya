b92901d88a365cb021f1b73906242ef8b90c63e1
ourgroupwork <ishapun09@gmail.com>
2023-03-11 14:39:17 +0545

    Two branch merged

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectdb } from "./.config/db.config.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -7,3 +8,5 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`server is running at port ${PORT}`);
 });
+
+connectdb();
